4a962d2e1e204f869ed0c51b2403a161f236818b
Chinseok Lee <allieuslee@gmail.com>
2016-06-17 15:55:31 +0900

    post.models.Comment 댓글 모델 정의

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
