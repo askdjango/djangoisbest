aeaa88128a21b53a0fc21bf3d8bda96cbbc5d8d8
Chinseok Lee <allieuslee@gmail.com>
2016-06-17 15:08:34 +0900

    blog.models.Post 모델을 admin 에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['title']
+
