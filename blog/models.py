92c0d2724193e414827993678ce45684b427462e
Chinseok Lee <allieuslee@gmail.com>
2016-06-17 14:56:58 +0900

    blog.models.Post 모델 구현

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
