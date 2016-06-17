cb84b7d1767b605a87984e0efb5aa371513ab921
Chinseok Lee <allieuslee@gmail.com>
2016-06-17 16:04:51 +0900

    blog comment_new 뷰 구현 : CreateView CBV 활용

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<post_pk>\d+)/comments/new/$', views.comment_new, name='comment_new'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from blog.models import Post
+from django.shortcuts import get_object_or_404, render, resolve_url
+from django.views.generic import ListView, DetailView, CreateView
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        return super(CommentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return resolve_url('blog:post_detail', self.kwargs['post_pk'])
+
+comment_new = CommentCreateView.as_view()
 
