9ffef00e0d22d5dc3925a5f0d53a4d3cf8bace6b
Chinseok Lee <allieuslee@gmail.com>
2016-06-17 18:15:25 +0900

    포스팅 지도위치를 post_detail 에 표시

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,18 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def lat(self):
+        if self.latlng:
+            return self.latlng.split(',')[0]
+        return None
+
+    @property
+    def lng(self):
+        if self.latlng:
+            return self.latlng.split(',')[1]
+        return None
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
